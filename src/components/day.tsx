981af6654f2bc31c8816b6c5f06388f922521390
Patrice Sacuto <patrice.sacuto@gmail.com>
2022-12-03 13:14:36 +0100

    Layout minor change

--- src/components/day.tsx
+++ src/components/day.tsx
@@ -5,7 +5,7 @@ interface DayProps {
   implementation: (input: string) => string;
 }
 
-export function Day({name, implementation}: DayProps) {
+export function Day({ name, implementation }: DayProps) {
   const [dayInput, setDayInput] = useState("");
   const [result, setResult] = useState("");
   const [opened, setOpened] = useState(false);
@@ -18,14 +18,16 @@ export function Day({name, implementation}: DayProps) {
     return (
       <div>
         <h2>{name}</h2>
-        <textarea
-          cols={42}
-          rows={21}
-          onChange={(e) => setDayInput(e.currentTarget.value)}
-        ></textarea>
+        <div>
+          <textarea
+            cols={42}
+            rows={21}
+            onChange={(e) => setDayInput(e.currentTarget.value)}
+          ></textarea>
+        </div>
         <button onClick={execute} disabled={!dayInput}>Execute</button>
 
-        {result && <span>Result: {result}</span>}
+        {result && <div>Result: {result}</div>}
         <button onClick={() => setOpened(false)}>CLOSE</button>
       </div>
     );
